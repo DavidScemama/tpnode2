25db6759cdc01f9b3378dee8c03de932f2df3f81
DavidScemama <74009994+DavidScemama@users.noreply.github.com>
2022-11-30 15:31:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/rendu2', (rep, res)=>{
-    const fs = require('fs')
-    const csv = require('csv-parser')
-    const resultats = [];
-    const download = require('download');
-    const unzip = require('unzip-stream')
-    var i = 0;
-    var countTrue = 0;
+var express = require('express');
+var app = express();
+var port = 3000;
 
-    download('https://files.data.gouv.fr/insee-sirene/StockEtablissementLiensSuccession_utf8.zip', 'data').then(() => {
-        fs.createReadStream('data/StockEtablissementLiensSuccession_utf8.zip').pipe(unzip.Parse()).on('entry', function (entry) {
-            const fileName = entry.path;
-            const type = entry.type;
-            const size = entry.size;
-            if (fileName === "Data.csv") {
-                entry.pipe(csv()).on('data', (data) => resultats.push(data)).on('end', () => {
-    resultats.forEach(element => {
-        if(element.transfertSiege == 'true'){
-            countTrue++;
-        }
-        i++;
-    });
-    let pourcent = countTrue/i*100;
-    res.send(`Le pourcentage de société qui ont transféré leur siège social deouis le 1er Novembre 2022 est de ${pourcent.toFixed(3)}%`)
-  })
-            } else {
+app.get('/tp', function (req, res) {
+    var telechargement = require('download');
+    var fs = require('fs');
+    var csv = require('csv-parser');
+    var unzip = require('unzip-stream');
+    var transfert = 0;
+    var total = 0;
+    telechargement('https://files.data.gouv.fr/insee-sirene/StockEtablissementLiensSuccession_utf8.zip', 'data').then(function () {
+        fs.createReadStream('data/Data.zip').pipe(unzip.Parse()).on('entry', function (entry) {
+            var file = entry.path;
+            
+            if (file === "Data.csv") {
+                entry.pipe(csv())
+                    .on('data', function (data) { 
+
+                        total = total + 1 ; 
+
+                        if (data.transfertSiege == 'true'){
+                            transfert = transfert + 1;
+                        } 
+                     })
+                    .on('end', function () {
+                    var percent = transfert / total * 100;
+                    var resultats = percent.toFixed(3);
+                    res.send(`Le pourcentage de société qui ont transféré leur siège social depuis le 1er Novembre 2022 est de ${pourcent.toFixed(3)}%`);
+                });
+            }
+            else {
                 entry.autodrain();
             }
         });
-})
-})
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
